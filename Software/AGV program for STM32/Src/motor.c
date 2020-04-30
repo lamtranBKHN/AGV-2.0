1364d2014597ac4aa9888c3511d33b7112d91f56
Lâm Trần <61534619+lamtranBKHN@users.noreply.github.com>
2020-04-30 23:58:29 +0700

    Update motor.c

--- Software/AGV program for STM32/Src/motor.c
+++ Software/AGV program for STM32/Src/motor.c
@@ -5,38 +5,30 @@
 #include "motor.h"
 extern TIM_HandleTypeDef htim3;
 void motorLeftForward(uint32_t speed) {
-	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_4);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
 	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, speed);
-	HAL_Delay(1);
 }
 void motorLeftBackward(uint32_t speed) {
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_4);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
 	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, speed);
-	HAL_Delay(1);
 }
 void motorRightForward(uint32_t speed) {
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
-	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_4);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
 	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, speed);
-	HAL_Delay(1);
 }
 void motorRightBackward(uint32_t speed) {
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
-	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_4);
-	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
-	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, speed);
-	HAL_Delay(1);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, speed);
 }
